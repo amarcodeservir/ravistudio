9e2b879de60eed92c8c21690b411b1590b549f40
A-yadav07 <ayadav02072001@gmail.com>
2024-12-10 17:03:59 +0530

    save

--- src/Home/Index.jsx
+++ src/Home/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ImageSlider from "../ImageSlider/ImageSlider";
 import Discription from "./Discription";
+import Overservice from "../OverServices/Overservice";
 
 function Index() {
   return (
     <div>
       <ImageSlider />
       <Discription />
+      <Overservice />
     </div>
   );
 }
