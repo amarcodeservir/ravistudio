28eec93c92446cf010b2de7ee6fefd6da8226b2c
Anand Bahadur Yadav <75464458+ANAND2001-A@users.noreply.github.com>
2024-12-25 11:41:36 +0530

    save anand

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Photography from "./Components/Photography/Photography";
 import Birthday from "./Components/HeaderPhotography/Birthday";
 import ContactForm from "./Components/Contact/ContactForm";
 import AdminDashboard from "./Components/Admin/AdminDashboard";
-// testing
+// testing development
 function App() {
   const { isAdmin, isUserLogin } = AdminDashboard();
 
